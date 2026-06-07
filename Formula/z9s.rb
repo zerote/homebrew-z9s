e130f872e009353756b858d0f12fee95b2cc933b
Z9S Automation <dev@z9s.dev>
2026-06-07 20:46:50 +0000

    fix: correct SHA256 checksum for v0.54.3 release

--- Formula/z9s.rb
+++ Formula/z9s.rb
@@ -2,7 +2,7 @@ class Z9s < Formula
   desc "Unified Kubernetes CLI with FluxCD support and metrics dashboard"
   homepage "https://github.com/zerote/z9s"
   url "https://github.com/zerote/z9s/archive/refs/tags/v0.54.3.tar.gz"
-  sha256 "a5d904045781464e68b9699d11c9895ec76147e8e15294bc53622c1b0a83e372"
+  sha256 "04bd6f4147fd141360e23e13d34db8643eb8f3d482c6f31ec5001b5a8bb9a29c"
   license "Apache-2.0"
   
   depends_on "go" => :build
